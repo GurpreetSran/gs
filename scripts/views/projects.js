bd6e7e59566b09b1b31e77e8c2453cb7705c1dd9
Gurpreet Singh <gurpreet.singh@trinnylondon.com>
2024-01-08 13:56:26 +0000

    Updates

--- scripts/views/projects.js
+++ scripts/views/projects.js
@@ -1,162 +1,172 @@
-var React = require('react'),
-	projectsData = require('./../projects'),
-	ProjectsRow = require('./projects_row'),
-	itemsInRow = 3,
-	noOfDefaultRows = 2,
-	projects = [],
-	currentProjects = [],
-	currentPageRows,
-	currentParamKey,
-	Projects,
-	noProjectAvailable = false;
+var React = require("react"),
+  projectsData = require("./../projects"),
+  ProjectsRow = require("./projects_row"),
+  itemsInRow = 3,
+  noOfDefaultRows = 2,
+  projects = [],
+  currentProjects = [],
+  currentPageRows,
+  currentParamKey,
+  Projects,
+  noProjectAvailable = false;
 
-	Projects = React.createClass({
-	//Comarison is not case sensitive, this method returns title as saved in db.	
-	getActualTitle: function(title) {
-		var skills,
-			i,
-			projectsList;
+Projects = React.createClass({
+  //Comarison is not case sensitive, this method returns title as saved in db.
+  getActualTitle: function (title) {
+    var skills, i, projectsList;
 
-		projectsList = projectsData.getByKey(title);	
+    projectsList = projectsData.getByKey(title);
 
-		if(projectsList.length > 0) {
-			skills = projectsList[0].skills;
+    if (projectsList.length > 0) {
+      skills = projectsList[0].skills;
 
-			for(i=0; i < skills.length; i++ ) {
-				if(title.toLowerCase() === skills[i].toLowerCase()) {
-					return skills[i];
-				}
-			} 
-		} 
+      for (i = 0; i < skills.length; i++) {
+        if (title.toLowerCase() === skills[i].toLowerCase()) {
+          return skills[i];
+        }
+      }
+    }
 
-		return false;
-	},
+    return false;
+  },
 
-	componentWillMount: function() {
-		this.componentConfig(this.props);
-	}, 
+  componentWillMount: function () {
+    this.componentConfig(this.props);
+  },
 
-	componentWillReceiveProps: function(nextProps){
-		this.componentConfig(nextProps);
-	},
+  componentWillReceiveProps: function (nextProps) {
+    this.componentConfig(nextProps);
+  },
 
-	componentConfig: function(props) {
-		var rows = [],
-			loadMoreIsVisible = true,
-			i; 
+  componentConfig: function (props) {
+    var rows = [],
+      loadMoreIsVisible = true,
+      i;
 
-		if(props.params.key) {
-			currentParamKey = props.params.key;
-		}else {
-			currentParamKey = 'projects';
-		} 	
-		
-		projects = projectsData.getByKey(currentParamKey);	
+    if (props.params.key) {
+      currentParamKey = props.params.key;
+    } else {
+      currentParamKey = "projects";
+    }
 
-		if(projects.length === 0) {
-			noProjectAvailable = true;
-		} else {
-			noProjectAvailable = false;
-		}
+    projects = projectsData.getByKey(currentParamKey);
 
-		projectsData.currentState[currentParamKey] = noOfDefaultRows;		
-		currentProjects = projects.slice(); //Create a copy or array 
-		noOfDefaultRows = projectsData.currentState[currentParamKey] || noOfDefaultRows; 	
-	
-		for (i = 0; i < noOfDefaultRows; i++) {
-			if(currentProjects.length) {
-				rows.push(currentProjects.splice(0, itemsInRow));
-			} 
-		}  
+    if (projects.length === 0) {
+      noProjectAvailable = true;
+    } else {
+      noProjectAvailable = false;
+    }
 
-		currentProjects.length ? loadMoreIsVisible = true : loadMoreIsVisible = false;
+    projectsData.currentState[currentParamKey] = noOfDefaultRows;
+    currentProjects = projects.slice(); //Create a copy or array
+    noOfDefaultRows =
+      projectsData.currentState[currentParamKey] || noOfDefaultRows;
 
-		this.setState({rows: rows});  
-		this.setState({loadMoreBtn: loadMoreIsVisible})	
-	},
+    for (i = 0; i < noOfDefaultRows; i++) {
+      if (currentProjects.length) {
+        rows.push(currentProjects.splice(0, itemsInRow));
+      }
+    }
 
-	loadMoreProjects: function(e) {
+    currentProjects.length
+      ? (loadMoreIsVisible = true)
+      : (loadMoreIsVisible = false);
 
-		e.preventDefault();
-		var addRow = this.state.rows;
-	
-		if(currentProjects.length) {
-			currentPageRows++;
-			addRow.push(currentProjects.splice(0, itemsInRow));
-			this.setState({rows: addRow});  
-		} 
+    this.setState({ rows: rows });
+    this.setState({ loadMoreBtn: loadMoreIsVisible });
+  },
 
-		if(!currentProjects.length) {
-			this.setState({loadMoreBtn: false})
-		}	
-	},
+  loadMoreProjects: function (e) {
+    e.preventDefault();
+    var addRow = this.state.rows;
 
-	render: function() {
+    if (currentProjects.length) {
+      currentPageRows++;
+      addRow.push(currentProjects.splice(0, itemsInRow * 2));
+      this.setState({ rows: addRow });
+    }
 
-		var projectUrl, 
-			header = '';
+    if (!currentProjects.length) {
+      this.setState({ loadMoreBtn: false });
+    }
+  },
 
-		if(currentParamKey === 'projects') {
-			projectUrl = '#/project';
-		} else {
-			header = currentParamKey;
-			projectUrl = '#/project/' + currentParamKey
-		} 	
+  render: function () {
+    var projectUrl,
+      header = "";
 
-		if(noProjectAvailable) {
-		return (
+    if (currentParamKey === "projects") {
+      projectUrl = "#/project";
+    } else {
+      header = currentParamKey;
+      projectUrl = "#/project/" + currentParamKey;
+    }
 
-				<div>
-					<div className="jumbotron">
-						<div className="container">
-							<h1 className="error">Navigation Error</h1>
-						</div>
-					</div>		
-					<div className="container">
-						<p className="error">
-							Invalid project id or URL
-						</p>	
-						<br />
-						<a className="linkStyle1" href={'#/projects'}>
-							<i className="fa fa-angle-right"></i><span>Projects</span>
-						</a>
-					</div> 
-				</div>
-			)
-		}
+    if (noProjectAvailable) {
+      return (
+        <div>
+          <div className="jumbotron">
+            <div className="container">
+              <h1 className="error">Navigation Error</h1>
+            </div>
+          </div>
+          <div className="container">
+            <p className="error">Invalid project id or URL</p>
+            <br />
+            <a className="linkStyle1" href={"#/projects"}>
+              <i className="fa fa-angle-right"></i>
+              <span>Projects</span>
+            </a>
+          </div>
+        </div>
+      );
+    }
 
-		return (
-			< div className="projects">
-				< div className = "jumbotron" >
-					< div className = "container" >
-						< h1 > { this.getActualTitle(header) ? this.getActualTitle(header) + ' Projects' : 'Projects'}   < /h1> 
-					< /div> 
-				< /div>
+    return (
+      <div className="projects">
+        <div className="jumbotron">
+          <div className="container">
+            <h1>
+              {" "}
+              {this.getActualTitle(header)
+                ? this.getActualTitle(header) + " Projects"
+                : "Projects"}{" "}
+            </h1>
+          </div>
+        </div>
 
-				< div className = "container" >					
-					{this.state.rows.map(function(row, i) {
-						return <ProjectsRow url={projectUrl} row={row} key={i} />
-					}.bind(this))}
-				
-				< /div> 
+        <div className="container">
+          {this.state.rows.map(
+            function (row, i) {
+              return <ProjectsRow url={projectUrl} row={row} key={i} />;
+            }.bind(this)
+          )}
+        </div>
 
-				< div className = "container text-center" >	
-					<a id="loadMore" className= {this.state.loadMoreBtn ? 'linkStyle1' : 'hide'} 
-						onClick = {this.loadMoreProjects}
-						role="button" > <i className="fa fa-angle-down"></i><span>Load More Projects</span> 
-					</a>
-					<br />
-					<br />
-					
-					<div className="contact-me-link">
-						<a className="linkStyle1" href="#/contact">
-							<i className="fa fa-angle-right"></i><span>Contact</span>
-						</a>
-					</div>
-				</div>
-			< /div>
-		);	
-	}
+        <div className="container text-center">
+          <a
+            id="loadMore"
+            className={this.state.loadMoreBtn ? "linkStyle1" : "hide"}
+            onClick={this.loadMoreProjects}
+            role="button"
+          >
+            {" "}
+            <i className="fa fa-angle-down"></i>
+            <span>Load More Projects</span>
+          </a>
+          <br />
+          <br />
+
+          <div className="contact-me-link">
+            <a className="linkStyle1" href="#/contact">
+              <i className="fa fa-angle-right"></i>
+              <span>Contact</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  },
 });
 
 module.exports = Projects;
