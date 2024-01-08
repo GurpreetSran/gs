bbc6ea3da6ed45979e01355bd37cfb1a8c7b5bfb
Gurpreet Singh <gurpreet.singh@trinnylondon.com>
2024-01-08 10:57:05 +0000

    update description

--- scripts/views/about.js
+++ scripts/views/about.js
@@ -12,7 +12,7 @@ var About = React.createClass({
         <div className="container">
           <p>
             As a seasoned professional, I bring forth a wealth of experience
-            coupled with exceptional organizational skills and an adaptable
+            coupled with exceptional organisational skills and an adaptable
             mindset. Over the course of more than a decade, I have cultivated my
             expertise while collaborating with esteemed clients and have
             fortified my knowledge through a diverse educational background.
@@ -26,7 +26,7 @@ var About = React.createClass({
             implement optimal solutions tailored to client requirements.
           </p>
           <p>
-            I thrive in environments that prioritize commitment and enthusiasm
+            I thrive in environments that prioritise commitment and enthusiasm
             for technology. Such settings not only bring out the best in me but
             also provide an ideal platform to leverage my technical proficiency.
             My unwavering passion for delivering excellence, coupled with a
@@ -48,12 +48,11 @@ var About = React.createClass({
                 <li className="list-group-item">Redux, Mobx, Sagas</li>
                 <li className="list-group-item">Node.js</li>
                 <li className="list-group-item">GraphQL</li>
-                <li className="list-group-item">Cypress - E2E testing</li>
-                <li className="list-group-item">Jest - Unit testing</li>
-                <li className="list-group-item">React testing library</li>
-                <li className="list-group-item">Express and Hapi Server</li>
-                <li className="list-group-item">HTML(5)</li>
-                <li className="list-group-item">CSS(3)</li>
+                <li className="list-group-item">
+                  Jest, React testing library, Enzyme, Jasmine, Cypress
+                </li>
+                <li className="list-group-item">Express, Hapi Server</li>
+                <li className="list-group-item">HTML(5), CSS(3)</li>
                 <li className="list-group-item">Python</li>
                 <li className="list-group-item">jQuery</li>
                 <li className="list-group-item">Material UI</li>
@@ -63,32 +62,31 @@ var About = React.createClass({
               </ul>
             </div>
             <div className="col-md-4">
-              <h3>Tools</h3>
+              <h3>Tools Used</h3>
               <ul className="list-group">
+                <li className="list-group-item">
+                  Responsive Design (Fluid Grids and Media Queries)
+                </li>
+                <li className="list-group-item">
+                  CSS preprocessors (SASS, LESS)
+                </li>
                 <li className="list-group-item">Docker</li>
-                <li className="list-group-item">Contentful - CMS</li>
                 <li className="list-group-item">
-                  CSS preprocessor (Sass, LESS){" "}
+                  Contentful CMS, Commerce Tools, Escenic
                 </li>
                 <li className="list-group-item">
-                  Circle CI, Concourse (CI/CD)
+                  Circle CI, Concourse (CI/CD), Teamcity, Jenkins
                 </li>
-                <li className="list-group-item">Humio and Datadog</li>
-                <li className="list-group-item">Redis - Caching</li>
+                <li className="list-group-item">Humio, Datadog, Sentry</li>
+                <li className="list-group-item">Redis (Caching)</li>
                 <li className="list-group-item">Web Sockets</li>
-                <li className="list-group-item">Bit - React Components</li>
-                <li className="list-group-item">Story Book - React</li>
+                <li className="list-group-item">Story Book, Bit - React</li>
                 <li className="list-group-item">Agile Methodologies</li>
-                <li className="list-group-item">NPM</li>
                 <li className="list-group-item">Figma, InVision</li>
-                <li className="list-group-item">Git and SVN</li>
-                <li className="list-group-item">Sentry</li>
+                <li className="list-group-item">Git, SVN</li>
                 <li className="list-group-item">Jira</li>
                 <li className="list-group-item">
                   Vite, Webpack, Require , Grunt and Browserify
-                </li>
-                <li className="list-group-item">
-                  Responsive Design, Fluid Grids and Media Queries
                 </li>
               </ul>
             </div>
@@ -99,13 +97,10 @@ var About = React.createClass({
                 <li className="list-group-item">AWS and GCP</li>
                 <li className="list-group-item">Serverless</li>
                 <li className="list-group-item">JWT</li>
-                <li className="list-group-item">DynamoDB</li>
-                <li className="list-group-item">Web3</li>
                 <li className="list-group-item">Flask</li>
-                <li className="list-group-item">Crypto Currency</li>
-                <li className="list-group-item">BigQuery</li>
-                <li className="list-group-item">Elastic Search</li>
-                <li className="list-group-item">MongoDB</li>
+                <li className="list-group-item">Web3, Crypto, BlockChain</li>
+                <li className="list-group-item">BigQuery, Elastic Search</li>
+                <li className="list-group-item">MongoDB, SQL, DynamoDB</li>
               </ul>
             </div>
           </div>
